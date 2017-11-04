1fe98a1340e7eb61c8ea47491c58b02c96209504
Krzysztof Wawer <krzysztof.wawer@gmail.com>
2017-11-05 00:06:51 +0100

    Add first version of RuboCop config rules

--- lib/alterans/stylecheck.rb
+++ lib/alterans/stylecheck.rb
@@ -1,7 +1,19 @@
+require "rubocop-stylecheck"
+
 require "alterans/stylecheck/version"
 
 module Alterans
   module Stylecheck
-    # Your code goes here...
+    class << self
+      def root
+        Gem::Specification.find_by_name("alterans-stylecheck").gem_dir
+      end
+
+      def template_path
+        File.join(root, "template", "rubocop.yml")
+      end
+    end
   end
 end
+
+Rubocop::Stylecheck.config_path = Alterans::Stylecheck.template_path
